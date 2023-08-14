001c9bf699e9e00894b99f00e9a68a966b5d77f6
redsonfarias <redson.filho23@gmail.com>
2023-08-14 14:35:44 -0300

    change hook function

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-export const useDebounce = (value: string, delay: number) => {
+export function useDebounce(value: string, delay: number) {
   const [debouncedValue, setDebouncedValue] = useState(value)
 
   useEffect(() => {
